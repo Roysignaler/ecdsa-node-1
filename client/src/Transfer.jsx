c904bc2a722ce8d37d639761e1704e620fe05430
Roysignaler <98877973+Roysignaler@users.noreply.github.com>
2023-03-24 04:45:58 +0100

    Update Transfer.jsx

--- client/src/Transfer.jsx
+++ client/src/Transfer.jsx
@@ -1,15 +1,48 @@
 import { useState } from "react";
 import server from "./server";
 
-function Transfer({ address, setBalance }) {
+// Cryptography imports
+import { keccak256 } from 'ethereum-cryptography/keccak';
+import { utf8ToBytes } from 'ethereum-cryptography/utils';
+
+import * as secp from '@noble/secp256k1';
+
+function Transfer({ address, setBalance, privateKey}) {
   const [sendAmount, setSendAmount] = useState("");
   const [recipient, setRecipient] = useState("");
-
+  
   const setValue = (setter) => (evt) => setter(evt.target.value);
+
 
   async function transfer(evt) {
     evt.preventDefault();
+    
+   
+    // 3 functions to create a signature, with private key as input
 
+    //1 Function to convert message to bytes
+    function messageInBytes(message) {
+      const msgInBytes = utf8ToBytes(message)
+      console.log('In bytes msg is:', msgInBytes)
+      return msgInBytes
+    }
+    const rawMsg = recipient;
+    const msgInBytes = messageInBytes(recipient);
+
+    //2 Function to hash value of message
+    function hashKeccak(input) {
+      const hash = keccak256(input);
+      console.log('The hash is:', hash);
+      return hash
+    }
+    const message = hashKeccak(msgInBytes)
+
+    // 3 calculate a signature with the given private key
+    let signature = await secp.sign(message, privateKey, { recovered: true } );
+    console.log("The signature is: " + signature);
+    
+    
+ 
     try {
       const {
         data: { balance },
@@ -17,6 +50,8 @@ function Transfer({ address, setBalance }) {
         sender: address,
         amount: parseInt(sendAmount),
         recipient,
+        rawMsg,
+        signature,
       });
       setBalance(balance);
     } catch (ex) {
@@ -46,9 +81,11 @@ function Transfer({ address, setBalance }) {
         ></input>
       </label>
 
+
       <input type="submit" className="button" value="Transfer" />
     </form>
   );
+
 }
 
 export default Transfer;
